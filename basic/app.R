76eda7f2f7fe4d841ec94cd4244cef616cdd5032
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-01-08 13:32:01 +0100

    feat: add basic reload example

---
+++ basic/app.R
@@ -0,0 +1,25 @@
+box::use(
+  shiny[actionButton, fluidPage, observeEvent, renderText, shinyApp, textOutput],
+)
+
+box::use(./module)
+
+options(shiny.autoreload = TRUE)
+
+env <- environment() # Store environment where box::use is called
+shiny:::autoReloadCallbacks$register(function() {
+  evalq(box::reload(module), env) # Call reload within the stored environment
+})
+
+shinyApp(
+  fluidPage(
+    actionButton("print", "print"),
+    textOutput("t")
+  ),
+  function(input, output, session) {
+    observeEvent(input$print, ignoreNULL = FALSE, {
+      print(module$foo)
+      output$t <- renderText(module$foo)
+    })
+  }
+)
